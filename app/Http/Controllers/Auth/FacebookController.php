8180f6b73adb4030884110a5a6e02e27c8b3f8d9
miamius <jairo.laupa@gmail.com>
2017-08-06 17:53:41 -0300

    ´Inicializando´

---
+++ app/Http/Controllers/Auth/FacebookController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jlaupa
+ * Date: 6/8/2017
+ * Time: 16:54
+ */
+namespace App\Http\Controllers\Auth;
+
+use Socialite;
+
+class FacebookController extends Controller
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('github')->user();
+
+        // $user->token;
+    }
+}
